4e9d2205a365d79c00e78e08d7fb526d1511dc4c
Lukáš Zoubek <zoublu@email.cz>
2022-10-08 10:10:24 +0200

    delete unused title parameter

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
-
-
